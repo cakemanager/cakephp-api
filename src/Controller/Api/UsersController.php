d0f4a8df8ca3c8cc5fba04cb84c947cd08bd8d17
Leonardo Ruhland <leoruhland@gmail.com>
2015-12-01 18:25:32 -0200

    Fixed JWT namespace
    See https://github.com/firebase/php-jwt/pull/59 for details.

--- src/Controller/Api/UsersController.php
+++ src/Controller/Api/UsersController.php
@@ -5,6 +5,7 @@ use Api\Controller\ApiControllerTrait;
 use Api\Controller\AppController;
 use Cake\Network\Exception\UnauthorizedException;
 use Cake\Utility\Security;
+use \Firebase\JWT\JWT;
 
 /**
  * Users Controller
@@ -31,7 +32,7 @@ class UsersController extends AppController
 
         $this->set('data', [
             'user' => $user,
-            'token' => $token = \JWT::encode([
+            'token' => $token = JWT::encode([
                 'id' => $user['id'],
                 'user' => $user,
                 'exp' => time() + 604800
